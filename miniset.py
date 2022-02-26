1e31436aaf6ed5621c018a5fd42b1d78973722a7
Fork my Git <45640841+Prometheos2@users.noreply.github.com>
2022-02-26 17:54:29 +0100

    Beef up the data sample

--- miniset.py
+++ miniset.py
@@ -4,9 +4,9 @@ import random
 import shutil
 
 dataset = os.path.normpath('C:\\Users\\user\\Desktop\\train\\train')
-miniset = os.path.normpath('C:\\Users\\user\\Desktop\\train\\mini\\')
+miniset = os.path.normpath('C:\\Users\\user\\Desktop\\train\\small\\')
 
 files = glob.glob(dataset + os.sep + "*.flac")
-sample = random.sample(files, 1000)
+sample = random.sample(files, 10000)
 
 [shutil.copy(file, miniset) for file in sample]
