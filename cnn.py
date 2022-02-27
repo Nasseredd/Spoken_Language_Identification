5ba4dca2a7125501c7ef27729d24b6b37b919ff0
Fork my Git <45640841+Prometheos2@users.noreply.github.com>
2022-02-27 04:12:15 +0100

    Refactor train & val w/ starmap

--- cnn.py
+++ cnn.py
@@ -1,5 +1,5 @@
 import itertools
-import os 
+import os
 
 import numpy as np
 import pandas as pd
@@ -35,6 +35,10 @@ class ConvNet(nn.Module):
         self.fc = nn.Linear(32*32*107, NUM_CLASSES)
         self.act = nn.Softmax(dim=1)
 
+        # Loss and optimizer (initialized during training)
+        self.criterion = None
+        self.optimizer = None
+
     def forward(self, x):
         x = x.unsqueeze(1)
         out = self.layer1(x)
@@ -45,66 +49,66 @@ class ConvNet(nn.Module):
         return out
 
     def train_model(self, train_loader: DataLoader, validation_loader: DataLoader = None):
+        # Send model (self) to DEVICE
         self = self.to(DEVICE)
+
+        # Store losses into Numpy Array
         losses = np.zeros((self.num_epochs, 2))
 
         # Loss and optimizer
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(
+            self.parameters(), lr=self.learning_rate)
 
-        # Training iteration (for map())
-        def train_iter(mel_spectro, lang): return self.__train_iteration(
-            criterion, optimizer, mel_spectro, lang)
-
-        # Validation function: execute validation() or return None
-        def validate(): return self.validation(
-            criterion, validation_loader) if validation_loader else lambda: None
+        # Shortcut Function for validation
+        def validate(): return self.validation(validation_loader)
 
         # Train the model
         for epoch in range(1, self.num_epochs + 1):
             # Perform training and validation.
-            loss = np.mean(list(itertools.starmap(train_iter, train_loader)))
+            train_loss = np.mean(
+                list(itertools.starmap(self.train_iter, train_loader)))
             val_loss = validate()
 
             # Print & store losses
-            print('Epoch [{}/{}], Loss: {:.4f}, Val. Loss: {:.4f}'.format(epoch, self.num_epochs, loss, val_loss))
-            losses[epoch] = [loss, val_loss]
+            print('Epoch [{}/{}], Loss: {:.4f}, Val. Loss: {:.4f}'.format(epoch,
+                  self.num_epochs, train_loss, val_loss))
+            losses[epoch] = [train_loss, val_loss]
 
             # Create model checkpoint
             self.create_checkpoint(epoch)
 
+        # Store losses into CSV file.
         losses_df = pd.DataFrame(losses, columns=["Training", "Validation"])
         losses_df.to_csv("training_losses.csv")
 
-    def __train_iteration(self, criterion, optimizer, mel_spectro, lang):
+    def forward_pass(self, mel_spectro, expected):
         mel_spectro = mel_spectro.to(DEVICE)
-        lang = lang.to(DEVICE)
-        lang = lang.squeeze()
+        expected = expected.to(DEVICE)
+        expected = expected.squeeze()
 
         # Forward pass
-        outputs = self(mel_spectro)
-        loss = criterion(outputs, lang)
+        predicted = self(mel_spectro)
+        return self.criterion(predicted, expected)
+
+    def train_iter(self, mel_spectro, lang):
+        loss = self.forward_pass(mel_spectro, lang)
 
         # Backward and optimize
-        optimizer.zero_grad()
+        self.optimizer.zero_grad()
         loss.backward()
-        optimizer.step()
+        self.optimizer.step()
 
         return loss.item()
 
-    def validation(self, criterion, validation_loader):
-        self.eval()
-        loss = 0
+    def validation(self, validation_loader):
+        self.eval()  # Activate evaluation mode
         with torch.no_grad():
-            for mel_spectro, lang in validation_loader:
-                mel_spectro = mel_spectro.to(DEVICE)
-                lang = lang.to(DEVICE)
-                lang = lang.squeeze()
-
-                out = self(mel_spectro)
-                loss += criterion(out, lang)
-        self.train()
-        return loss / len(validation_loader)
+            def validation_iter(x, y): return self.forward_pass(x, y).item()
+            loss = np.mean(list(itertools.starmap(
+                validation_iter, validation_loader)))
+        self.train()  # Activate training mode
+        return loss
 
     def create_checkpoint(self, epoch):
         path = os.path.join('checkpoints', f'model_{epoch}.ckpt')
